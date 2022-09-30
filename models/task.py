20df973937a3351c494d2f0ccbc063d23a220392
hawier-dev <contact@hawier.dev>
2022-09-30 14:55:59 +0200

    color detection task

--- models/task.py
+++ models/task.py
@@ -1,13 +1,6 @@
 from enum import Enum
 
 
-# class Task(Enum):
-#     RESIZE = 'resize'
-#     INVERT = 'invert'
-#     CONVERT = 'convert'
-#     COMPRESS = 'compress'
-
-
 class ImageExtension(Enum):
     JPEG = '.jpg'
     TIFF = '.tif'
@@ -43,3 +36,13 @@ class TaskCompress(Task):
     def __init__(self, quality=90):
         super().__init__()
         self.quality = quality
+
+
+class TaskColorDetection(Task):
+    def __init__(self, from_color: str, to_color: str, save_mask: bool, save_shp: bool, save_geojson: bool):
+        super().__init__()
+        self.from_color = from_color
+        self.to_color = to_color
+        self.save_mask = save_mask
+        self.save_shp = save_shp
+        self.save_geojson = save_geojson
